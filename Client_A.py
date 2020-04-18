ec0628e283571709ee82121339ab9dc156037a7d
mohbharti <moh16247@iiitd.ac.in>
2020-04-18 19:19:33 +0530

    Communication System created
    Client A and Client B establish communication and exchange first keys and then message.

--- Client_A.py
+++ Client_A.py
@@ -1,32 +1,103 @@
 import socket
 import hashlib
 import hmac
-
+import random
 mod_p = 17359
 
+secret = 5544
+da = 12 #random.randint(2,mod_p-2)
+alpha = 31
+beta = (alpha**da)%mod_p
+
+def share_secret(A):
+    base_g = primitive(mod_p)
+    secret = (A**alpha) % mod_p
+    return secret
 
 def primitive(pri):
     p = pri - 2
     pr = pri
-
     for i in range(p, 2, -2):
         if (i ** (p + 1)) % pri == 1:
             pr = i
 
             break
-
     return pr
 
+
 def connect():
-    HOST = '127.0.0.1'  # The server's hostname or IP address
-    PORT = 65432        # The port used by the server
+
+    host = '127.0.0.1'  # The server's hostname or IP address
+
+    port = 65432        # The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall('Hello, world I am  daiku'.encode())
-        data = s.recv(1024)
+        s.bind((host,port))
+        s.listen(1)
+        conn, addr = s.accept()
 
-    print('Received :  ', repr(data))
+        y = 0
+        u = 0
+        with conn:
+            print('Connected by', addr)
+            while y<5:
+                if y == 0: # Sending the public key to Client B
+                    print("-----------Sending public key message--------------")
+                    message = str(alpha)+" "+str(beta)+" "+MAC(str(alpha).encode())+" "+MAC(str(beta).encode())
+                    conn.sendall(bytes(message.encode()))
+                    print(message)
+                    u = 1
+                elif y == 1: # Receiving Authenticate message
+                    data = conn.recv(1024)
+                    print("-------------Receiving Authentication message--------------- ")
+                    print(data.decode("utf-8"))
+                    if data.decode("utf-8") != "Got the key":
+                        break
+                    else:
+                        u = 1
+                elif y == 2:
+                    data = conn.recv(1024)
+                    st = decrypt(data.decode("utf-8"),mod_p)
+                    print("Received message: ",st)
+                elif y == 3:
+                    data = conn.recv(1024)
+                    st = decrypt(data.decode("utf-8"), mod_p)
+                    print("Received message: ",st)
+                else:
+                    data = conn.recv(1024)
+                    st = decrypt(data.decode("utf-8"), mod_p)
+                    print("Received message: ",st)
+                y += u
+
+        print("Connection closed")
+
+
+def MAC(message):
+    digester = hmac.new(bytes(secret), message, hashlib.sha1)
+    digest = digester.hexdigest()
+
+    return digest
+
+
+def decrypt(message,p):
+
+    dec_mess = message.split()
+    mess = dec_mess[0]
+    key = int(dec_mess[1])
+    has = dec_mess[2]
+    ori_mess = ""
+    for i in range(len(mess)):
+
+        ori_mess += chr(((ord(mess[i])%p)*(key**(p-1-da))%p)%p)
+
+    if MAC(ori_mess.encode()) == has:
+        return ori_mess
+    else:
+
+        return "Nothing"
+
 
 if __name__=='__main__':
-    
+    print("Starting the Communication.............. ")
+
+    connect()
--- Client_B.py
+++ Client_B.py
@@ -1,23 +1,35 @@
 import socket
 import hashlib
 import hmac
+import random
 
-prime = 17377
+
 
 mod_p = 17359
 
-prive = 23
+alpha = 0
+beta = 0
+secret = 5544
+
+
+
+def MAC(message):
+
+    digester = hmac.new(bytes(secret),message,hashlib.sha1)
+    digest = digester.hexdigest()
+    return digest
 
 def share_secret(B):
     base_g = primitive(mod_p)
-    share = (B**prive) % mod_p
+    secret = (B**alpha) % mod_p
 
+    return secret
 
 def primitive(pri):
     p = pri-2
     pr = pri
 
-    for i in range(p,2,-2):
+    for i in range(p, 2 , -2):
         if (i**(p+1))% pri == 1:
             pr = i
 
@@ -27,27 +39,76 @@ def primitive(pri):
     return pr
 
 
-
-
 def connect():
-    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+    try:
+        HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+        PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
-                print("Got it : ", repr(data))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+            s.connect((HOST,PORT))
+
+            i = 0
+            while i < 5:
+                if i == 0:
+                    data = s.recv(1024)
+                    print("Receiving the message:  ",repr(data))
+                    alpha, beta = verify(data.decode("utf-8"))
+                elif i == 1:
+                    print("-----------Sending the conformation message-----------")
+                    if alpha == 1 and beta == 1:
+                        s.sendall(bytes("Not Got".encode()))
+                    else:
+                        s.sendall(bytes("Got the key".encode()))
+                elif i == 2:
+                    print("Sending message: hello 1")
+                    s.sendall(bytes(encrypt("hello 1",alpha,mod_p,beta).encode()))
+                elif i == 3:
+                    print("Sending message: hello 2")
+                    s.sendall(bytes(encrypt("hello 2",alpha,mod_p,beta).encode()))
+                else:
+                    print("Sending message: hello 3")
+                    s.sendall(bytes(encrypt("hello 3",alpha,mod_p,beta).encode()))
+                i += 1
+
+            print("Closing Connection ")
+    except ConnectionAbortedError:
+        print()
+
+
+def encrypt(message, alpha, p, beta):
+    k = random.randint(2, p - 2)
+    enc_mess = ""
+    for i in range(len(message)):
+        enc_mess += chr((ord(message[i]) * (beta ** k)) % p)
+
+    hashes = MAC(message.encode())
+    enc_mess += " " + str((alpha ** k) % p)
+
+    payload = enc_mess + " " + hashes
+
+    return payload
+
+
+def verify(message):
+    mess = message.split()
+
+    if MAC(mess[0].encode()) == mess[2] and MAC(mess[1].encode()) == mess[3]:
+        alpha1 = int(mess[0])
+        beta1 = int(mess[1])
+
+        return alpha1,beta1
+
+    else:
+        alpha2 = 1
+        beta2 =1
+        return alpha2,beta2
 
 
 if __name__ == '__main__':
-    print("hrllo")
-    print(primitive(prime))
+    print("Communicating.................")
+    connect()
+
+
+
 
